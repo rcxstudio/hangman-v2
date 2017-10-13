30791b55b1d92582314298baaa6f4011b1093aab
HQ <5462117+Ropestar1@users.noreply.github.com>
2017-10-13 01:06:29 -0700

    Setting up with Sequelize.

--- controllers/html-routes.js
+++ controllers/html-routes.js
@@ -1,15 +1,16 @@
-var express = require("express");
+const express = require('express');
 
-var router = express.Router();
+const router = express.Router();
 
 // Import the model (burger.js) to use its database functions.
 // var burger = require("../models/burger.js");
 // var db = require('../models');
 
 // Create all our routes and set up logic within those routes where required.
-router.get("/", function(req, res) {
+router.get('/', function(req, res) {
 
 });
 
 // Export routes for server.js to use.
+// TODO: move to ES6
 module.exports = router;
