4f995cfa2c65c829e203241324ea0db19586f3c7
HQ <5462117+Ropestar1@users.noreply.github.com>
2017-10-28 00:31:22 -0700

    ES6 syntax for server.js

--- server.js
+++ server.js
@@ -1,13 +1,25 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const methodOverride = require('method-override');
+// const express = require('express');
+import express from 'express';
+// const bodyParser = require('body-parser');
+import bodyParser from 'body-parser';
+// const methodOverride = require('method-override');
+import methodOverride from 'method-override';
+// const mongoose = require("mongoose");
+import mongoose from 'mongoose';
+// const logger = require("morgan");
+import {logger} from 'morgan';
 
-const db = require('./models');
+//TODO: add mongo models
+// const User = require('..')
+
+// Set mongoose to leverage built in JavaScript ES6 Promises
+mongoose.Promise = Promise;
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
+app.use(logger("dev")); // what is this?
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.text());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
@@ -15,16 +27,24 @@ app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 app.use(methodOverride('_method'));
 
 app.use(express.static('public'));
+mongoose.connect("mongodb://localhost/hangman");
+// mongoose.connect("mongodb://heroku_n9jhvtvp:2ljmj0t4f8kvq1h8uvfli79tnc@ds151431.mlab.com:51431/heroku_n9jhvtvp");
+var db = mongoose.connection;
+
+db.on("error", function(error) {
+  console.log("Mongoose Error: ", error);
+});
+
+db.once("open", function() {
+  console.log("Mongoose connection successful.");
+});
+
 
 // Routes import
 const routes = require('./controllers/html-routes.js');
 // const routes = require('./controllers/api-routes.js');
 app.use('/', routes);
 
-// Syncing our sequelize models and then starting our Express app
-// =============================================================
-db.sequelize.sync({ force: true }).then(function() {
-  app.listen(PORT, function() {
-    console.log('App listening on PORT ' + PORT);
-  });
+app.listen(PORT, function() {
+  console.log("App running on port 3000!");
 });
