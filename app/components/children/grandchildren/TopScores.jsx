2b24f901e1cb2f5704af081d9c8093893ab7cc2a
HQ <5462117+Ropestar1@users.noreply.github.com>
2017-11-29 15:21:04 -0800

    TopScores component added

--- app/components/children/grandchildren/TopScores.jsx
+++ app/components/children/grandchildren/TopScores.jsx
@@ -4,7 +4,7 @@ import axios from 'axios';
 
 import helpers from '../../utils/helpers.js';
 
-class Streak extends React.Component {
+class TopScores extends React.Component {
   constructor(props){
     super(props);
   }
@@ -13,7 +13,7 @@ class Streak extends React.Component {
     return (
       <div className="panel panel-default">
         <div className="panel-heading">
-          <h3 className="panel-title ">Streak</h3>
+          <h3 className="panel-title ">Top 10 Scores</h3>
         </div>
         <div className="panel-body">
           {this.props.winStreak}
@@ -22,4 +22,4 @@ class Streak extends React.Component {
   )}
 }
 
-export default Streak;
+export default TopScores;
